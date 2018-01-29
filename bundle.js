5f6f527b89b5f3945568b2d556544684f9424ea3
karthik_uandme <karthik.b@uandme.org>
2018-01-29 15:13:00 +0530

    initial commit

--- bundle.js
+++ bundle.js
@@ -71,7 +71,7 @@ function component() {
     var element = document.createElement('div');
   
     // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = _.join(['Hello', 'world'], ' ');
+    element.innerHTML = _.join(['Hello', ''], ' ');
   
     return element;
   }
